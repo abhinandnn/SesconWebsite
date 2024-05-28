5f7b7dc0bcc8dac85da69e28dcad2b2ad66c530c
Abhinandan <abhinandnn21@gmail.com>
2024-05-29 04:07:51 +0530

    updated few changes

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import axios from '@/api/axios'
+import logo from '@/assets/logo.svg'
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        
+    </div>
   )
 }
 
 export default Home
 
-export async function getStaticProps() {
+// export async function getStaticProps() {
   
-    return {
-      props: {
-        posts,
-      },
-    }
-  }
+//     return {
+//       props: {
+//         posts,
+//       },
+//     }
+//   }
