4d24a73a48dc80faca634bcdf79058dc8906df72
Abhinandan <abhinandnn21@gmail.com>
2024-06-12 10:53:48 +0530

    added featured project cards

---
+++ src/components/projectCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function ProjectCard() {
+  return (
+    <div className='w-[20.2rem] h-[22.5rem] group bg-cover flex flex-col-reverse bg-[url("/img1.svg")]'>
+        <div className=''>
+        <div className=' relative bg-black group-hover:h-[8rem] transition-all ease-out bg-opacity-30 h-[22.5rem]'>
+            <div className='absolute bottom-0 p-[1.8rem] group-hover:py-[1rem]'>
+<div className='text-[1.375rem] text-white'>
+Great building of Senorita- A way to heaven 
+</div>
+<div className='text-[1rem] group-hover:hidden text-white'>
+Unknown printer took a galley of type and scrambled it to make a 
+</div>
+</div>
+</div>
+</div>
+    </div>
+  )
+}
+
+export default ProjectCard
