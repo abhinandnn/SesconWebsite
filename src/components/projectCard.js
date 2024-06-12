21ed0e35076e714caa4a142699ae1e42adf5747a
Abhinandan <abhinandnn21@gmail.com>
2024-06-13 03:30:45 +0530

    added button to projectCard

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -4,7 +4,10 @@ function ProjectCard() {
   return (
     <div className='w-[20.2rem] h-[22.5rem] group bg-cover flex flex-col-reverse bg-[url("/img1.svg")]'>
         <div className=''>
-        <div className=' relative bg-black group-hover:h-[8rem] transition-all ease-out bg-opacity-30 h-[22.5rem]'>
+        <div className=' relative bg-black group-hover:h-[8rem] transition-all ease-out bg-opacity-30 h-[22.5rem] flex flex-col items-center'>
+            <button className='w-[10.6rem] h-[3.37rem] rounded-[1.62rem] text-[1.25rem] p-0 bg-white group-hover:block absolute hidden top-[-7rem] items-center justify-center'>
+                View Details
+            </button>
             <div className='absolute bottom-0 p-[1.8rem] group-hover:py-[1rem]'>
 <div className='text-[1.375rem] text-white'>
 Great building of Senorita- A way to heaven 
