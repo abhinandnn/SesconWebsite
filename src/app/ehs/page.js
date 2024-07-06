285ce86c0d2833ec2619602d3108598b2bbeff5e
Som gupta <ssomvishwari786@gmail.com>
2024-07-06 15:16:28 +0530

    create ehs page

---
+++ src/app/ehs/page.js
@@ -0,0 +1,38 @@
+export default function EHS() {
+  return (
+    <div className="bg-white pt-[4.5rem] px-[4vw] flex justify-between">
+      <div className="text-[2.5rem] leading-[56px] w-[8rem] min-w-fit text-[#B33F0F] pt-1 ">
+        ENVIRONMENT HEALTH<br/> AND SAFETY PLOICY
+      </div>
+      <div className=" flex flex-col gap-6 w-[62.7%]">
+      <div className="text-[28px] pl-14">
+        We at SESCON BUILDERS are committed to provide Safe and Healthy working
+        environment with concern for our surrounding environment at our location
+        by taking adequate steps to comply with the environmental standards,
+        minimizing occupational risks and hazards. We shall strive to achieve
+        these objectives through :<br/> Comply with all relevant and applicable
+        statutory provisions pertaining to Environment Health & Safety .
+      </div>
+      <div className=" flex flex-col gap-6 w-full">
+      <div className="flex gap-6 items-center">
+            <div className=" flex-shrink-0"><img src="/ticks.svg"/></div>
+            <div className="text-[28px]">Provide and ensure regular use of personal protective equipment. Provide and ensure regular use</div>
+        </div>
+        <div className="flex gap-6 items-center">
+            <div className=" flex-shrink-0"><img src="/ticks.svg"/></div>
+            <div className="text-[28px]">Provide and ensure regular use of personal protective equipment.</div>
+        </div>
+        <div className="flex gap-6 items-center">
+            <div className=" flex-shrink-0"><img src="/ticks.svg"/></div>
+            <div className="text-[28px]">Provide and ensure regular use of personal protective equipment.</div>
+        </div>
+        <div className="flex gap-6 items-center">
+            <div className=" flex-shrink-0"><img src="/ticks.svg"/></div>
+            <div className="text-[28px]">Provide and ensure regular use of personal protective equipment.</div>
+        </div>
+      </div>
+      </div>
+     
+    </div>
+  );
+}
